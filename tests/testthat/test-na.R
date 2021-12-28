7706303ba54b737ec01e1b137521018157e68d6e
Lief <williamlief@gmail.com>
2021-12-28 14:31:29 -0800

    fixed test to pass for count_teachers

--- tests/testthat/test-na.R
+++ tests/testthat/test-na.R
@@ -1,7 +1,8 @@
 # https://stackoverflow.com/questions/60269017/using-testthat-to-check-all-variables-in-a-data-frame-for-na-values
 
-test_that("No Missing Values - excluding LA OH", {
+test_that("No Missing Values - excluding LA and OH, count teachers", {
   df <- dplyr::filter(evaluationDB, !state %in% c("LA", "OH"))
+  df <- dplyr::select(df, -count_teachers)
   res <- apply(df, 2, function(x) sum(is.na(x))>0)
   expect_true(all(!res),
               label = paste(paste(names(which(res)), collapse=", ")))
