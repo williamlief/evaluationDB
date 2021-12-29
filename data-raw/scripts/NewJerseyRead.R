4230f4f8f83fd553453aacc8e7210e3dc4e90235
Lief <williamlief@gmail.com>
2021-12-28 20:55:55 -0800

    Reformatatted NJ code

--- data-raw/scripts/NewJerseyRead.R
+++ data-raw/scripts/NewJerseyRead.R
@@ -32,28 +32,56 @@ raw[[3]] <- read_excel(paste(path, "NJDOE_STAFF_EVAL_1516.xlsx", sep = "/"), col
   mutate(year = 2016)
 
 nj <- bind_rows(raw) %>%
-  filter(SCHOOL_ID == "999" & CATEGORY == "TEACHERS" & LEA_NAME != "COUNTY TOTAL" & LEA_NAME != "statewide") %>%
-  select(DISTRICT_CODE,COUNTY_CODE,name=LEA_NAME,e1=INEFFECTIVE,e2=PARTIALLY_EFFECTIVE,e3=EFFECTIVE,e4=HIGHLY_EFFECTIVE,et=TOTAL,year) %>%
-  mutate(state = "NJ",
-         DISTRICT_CODE = ifelse(nchar(DISTRICT_CODE)==2,paste0(00,DISTRICT_CODE),DISTRICT_CODE),
-         DISTRICT_CODE = ifelse(nchar(DISTRICT_CODE)==3,paste0(0,DISTRICT_CODE),DISTRICT_CODE),
-         localid = paste0(COUNTY_CODE,DISTRICT_CODE),
-         name=tolower(name)) %>%
+  filter(
+    SCHOOL_ID == "999" &
+      CATEGORY == "TEACHERS" &
+      LEA_NAME != "COUNTY TOTAL" & LEA_NAME != "statewide"
+  ) %>%
+  select(
+    DISTRICT_CODE,
+    COUNTY_CODE,
+    name = LEA_NAME,
+    e1 = INEFFECTIVE,
+    e2 = PARTIALLY_EFFECTIVE,
+    e3 = EFFECTIVE,
+    e4 = HIGHLY_EFFECTIVE,
+    et = TOTAL,
+    year
+  ) %>%
+  mutate(
+    state = "NJ",
+    DISTRICT_CODE = ifelse(
+      nchar(DISTRICT_CODE) == 2,
+      paste0(00, DISTRICT_CODE),
+      DISTRICT_CODE
+    ),
+    DISTRICT_CODE = ifelse(
+      nchar(DISTRICT_CODE) == 3,
+      paste0(0, DISTRICT_CODE),
+      DISTRICT_CODE
+    ),
+    localid = paste0(COUNTY_CODE, DISTRICT_CODE),
+    name = tolower(name)
+  ) %>%
   mutate_at(vars(e1, e2, e3, e4, et),
-            function(x) as.numeric(gsub("\\*", NA, x))) %>% # * indicates data suppression
+            function(x)
+              as.numeric(gsub("\\*", NA, x))) %>% # * indicates data suppression
   rowwise %>%
-  mutate(sum=sum(e1,e2,e3,e4, na.rm=T),
-         nna = is.na(e1)+is.na(e2)+is.na(e3)+is.na(e4),
-         e1 = if_else(nna==1 & is.na(e1),0,e1),
-         e2_impute = if_else(nna==2 & is.na(e1) & is.na(e2), 1, 0),
-         e2 = if_else(nna==2 & is.na(e1) & is.na(e2), et-sum, e2),
-         e1 = if_else(et==sum&is.na(e1),0,e1),
-         e2 = if_else(et==sum&is.na(e2),0,e2),
-         e3 = if_else(et==sum&is.na(e3),0,e3),
-         e4 = if_else(et==sum&is.na(e4),0,e4),
-         es = et - sum(e1, e2, e3, e4, na.rm = T)) %>%
+  mutate(
+    sum = sum(e1, e2, e3, e4, na.rm = T),
+    nna = is.na(e1) + is.na(e2) + is.na(e3) + is.na(e4),
+    e1 = if_else(nna == 1 & is.na(e1), 0, e1),
+    e2_impute = if_else(nna == 2 &
+                          is.na(e1) & is.na(e2), 1, 0),
+    e2 = if_else(nna == 2 & is.na(e1) & is.na(e2), et - sum, e2),
+    e1 = if_else(et == sum & is.na(e1), 0, e1),
+    e2 = if_else(et == sum & is.na(e2), 0, e2),
+    e3 = if_else(et == sum & is.na(e3), 0, e3),
+    e4 = if_else(et == sum & is.na(e4), 0, e4),
+    es = et - sum(e1, e2, e3, e4, na.rm = T)
+  ) %>%
   mutate_if(is.numeric, as.integer) %>%
   select(state, year, localid, name, e1, e2, e3, e4, et, es, e2_impute) %>%
-  filter(name!="statewide")
+  filter(name != "statewide")
 
 write_csv(nj, "data-raw/clean_csv_files/NewJerseyEval.csv")
